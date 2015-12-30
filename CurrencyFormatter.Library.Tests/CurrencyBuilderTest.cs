9a25af5dfb7454a436039dd9f1780acde9b0b368
Ali Süleyman Topuz <atopuz@emakina.ch>
2015-12-30 15:05:06 +0200

    Project initialized.

---
+++ CurrencyFormatter.Library.Tests/CurrencyBuilderTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyFormatter.Library.Tests
+{
+    [TestClass]
+    public class CurrencyBuilderTest
+    {
+        [TestMethod]
+        public void Test_Currency()
+        {
+            var currencyBuilder = new CurrencyBuilder("'", ",", "CHF", "de-CH", 125000000000);
+
+            var result = currencyBuilder.Currency; 
+        }
+    }
+}
---
+++ CurrencyFormatter.Library/CurrencyBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyFormatter.Library
+{
+    public class CurrencyBuilder
+    {
+        public string CurrencyGroupSeperator { get; set; }
+        public string CurrencyDecimalSeperator { get; set; }
+        public string CurrencySymbol { get; set; }
+        public string CultureCode { get; set; }
+        public decimal? Amount { get; set; }
+        
+        public CurrencyBuilder()
+        {
+
+        }
+
+        public CurrencyBuilder(string currencyGroupSeperator, string currencyDecimalSeperator, string currencySymbol, string cultureCode, decimal amount)
+        {
+            CurrencyGroupSeperator = currencyGroupSeperator;
+            CurrencyDecimalSeperator = currencyDecimalSeperator;
+            CurrencySymbol = currencySymbol;
+            CultureCode = cultureCode;
+            Amount = amount;
+        }
+
+        public string Currency
+        {
+            get
+            {
+                if (!this.Amount.HasValue)
+                    return string.Empty;
+
+                var culture = new CultureInfo(CultureCode, false);
+
+                var numberFormatInfo = (NumberFormatInfo)culture.NumberFormat.Clone();
+
+                if (!string.IsNullOrEmpty(this.CurrencyGroupSeperator))
+                {
+                    numberFormatInfo.CurrencyGroupSeparator = this.CurrencyGroupSeperator;
+                }
+
+                if (!string.IsNullOrEmpty(this.CurrencyDecimalSeperator))
+                {
+                    numberFormatInfo.CurrencyDecimalSeparator = this.CurrencyDecimalSeperator;
+                }
+
+                if (!string.IsNullOrEmpty(this.CurrencySymbol))
+                {
+                    numberFormatInfo.CurrencySymbol = this.CurrencySymbol;
+                }
+
+
+                return Amount.Value.ToString("C", numberFormatInfo);
+            }
+        }
+    }
+}
